52b087c1f64ef46dec618d35bffceade716098b5
Kameliya Golova <jesyspa@gmail.com>
2026-09-03 10:25:16 +0000

    Republish student repo from admin (3be9447), through lecture 1

--- code/Course/Lecture01.lean
+++ code/Course/Lecture01.lean
@@ -11,39 +11,64 @@ import Course.KnightsAndKnaves
 namespace Lecture01
 set_option linter.defProp false
 
+#eval 2 + 2
+#check 2 + 2
+#check fun (x: Nat) => x + x
+#print Nat
+
 -- ## Truth and Implication
 
-theorem selfImpl {A : Prop} : A → A := sorry
+theorem trueIsTrue : True := True.intro
 
-theorem implConst {A B : Prop} : A → B → A := sorry
+theorem selfImpl {A : Prop} : A → A := fun a => a
 
-theorem implSelectSecond {A B C : Prop} : A → B → C → B := sorry
+theorem implConst {A B : Prop} : A → B → A :=
+    fun a _ => a
 
-theorem implModusPonens {A B : Prop} : A → (A → B) → B := sorry
+theorem implSelectSecond {A B C : Prop} : A → B → C → B :=
+    fun _ b _ => b
 
-theorem implTrans {A B C : Prop} : (A → B) → (B → C) → (A → C) := sorry
+theorem implModusPonens {A B : Prop} : A → (A → B) → B :=
+    fun a ab => ab a
+
+theorem implTrans {A B C : Prop} : (A → B) → (B → C) → (A → C) :=
+    fun ab bc a => bc (ab a)
 
 -- ## Conjunction
 
-theorem andIntro {A B : Prop} : A → B → A ∧ B := sorry
+theorem andIntro {A B : Prop} (a : A) (b : B) : A ∧ B := ⟨a, b⟩
+#print andIntro
 
-theorem andLeft {A B : Prop} : A ∧ B → A := sorry
+#print And.left
+theorem andLeft {A B : Prop} : A ∧ B → A := fun ab => ab.left
 
-theorem andRight {A B : Prop} : A ∧ B → B := sorry
+theorem andRight {A B : Prop} : A ∧ B → B := fun ⟨_, b⟩ => b
 
-theorem andSymm {A B : Prop} : A ∧ B → B ∧ A := sorry
+theorem andSymm {A B : Prop} : A ∧ B → B ∧ A := fun ⟨a, b⟩ => ⟨b, a⟩
 
-theorem andAssoc {A B C : Prop} : (A ∧ B) ∧ C → A ∧ (B ∧ C) := sorry
+theorem andAssoc {A B C : Prop} : (A ∧ B) ∧ C → A ∧ (B ∧ C) :=
+    fun ⟨⟨a, b⟩, c⟩ => ⟨a, ⟨b, c⟩⟩
 
 -- ## Disjunction
 
-theorem orIntroLeft {A B : Prop} : A → A ∨ B := sorry
+#print Or
+theorem orIntroLeft {A B : Prop} : A → A ∨ B :=
+    fun a => Or.inl a
 
-theorem orIntroRight {A B : Prop} : B → A ∨ B := sorry
+theorem orIntroRight {A B : Prop} : B → A ∨ B :=
+    fun b => .inr b
 
-theorem orElim {A B C : Prop} : (A → C) → (B → C) → A ∨ B → C := sorry
+theorem orElim {A B C : Prop} (f : A → C) (g : B → C) : A ∨ B → C :=
+    fun a_or_b =>
+        match a_or_b with
+        | .inl a => f a
+        | .inr b => g b
 
-theorem orSymm {A B : Prop} : A ∨ B → B ∨ A := sorry
+theorem orSymm {A B : Prop} : A ∨ B → B ∨ A
+--    fun aorb => aorb.elim (fun a => .inr a) (fun b => .inl b)
+--    fun aorb => match aorb with (part below)
+    | .inl a => .inr a
+    | .inr b => .inl b
 
 theorem orAssoc {A B C : Prop} : (A ∨ B) ∨ C → A ∨ (B ∨ C) := sorry
 
@@ -51,11 +76,15 @@ theorem andDistribOr {A B C : Prop} : A ∧ (B ∨ C) → A ∧ B ∨ A ∧ C :=
 
 -- ## False and negation
 
-theorem exFalso {A : Prop} : False → A := sorry
+#print False
+#check False.elim
+theorem exFalso {A : Prop} : False → A := nofun
 
-theorem notFalse : ¬False := sorry
+#print Not
+theorem notFalse : ¬False := fun fls => fls
 
-theorem absurdLive {A C : Prop} : A → ¬A → C := fun a na => sorry
+theorem absurdLive {A C : Prop} : A → ¬A → C :=
+    fun a na => False.elim (na a)
 
 theorem notNotIntro {A : Prop} : A → ¬¬A := fun a na => sorry
 
@@ -63,7 +92,7 @@ theorem notNotElim {A : Prop} : ¬¬A → A := fun nna => sorry
 
 -- ## Iff
 
-theorem iffRefl {A : Prop} : A ↔ A := sorry
+theorem iffRefl {A : Prop} : A ↔ A := ⟨fun x => x, fun x => x⟩
 
 theorem iffAsPair {A B : Prop} : (A ↔ B) → (A → B) ∧ (B → A) := sorry
 
@@ -82,16 +111,21 @@ Possible answers:
 -/
 
 /-- You meet A.  A says: "False is true" -/
-def answerFalseIsTrue : Answer ["A"] := sorry
+def answerFalseIsTrue : Answer ["A"] := verdict (A is-a knave)
 
 theorem puzzleFalseIsTrue (A : Prop) (hA : A ↔ False) :
-    claim% answerFalseIsTrue [A] := sorry
+    claim% answerFalseIsTrue [A] :=
+        fun a => hA.mp a
 
 /-- You meet A and B.  A says: "I am a knave or B is a knight." -/
-def answerKnaveOrKnight : Answer ["A", "B"] := sorry
+def answerKnaveOrKnight : Answer ["A", "B"] :=
+    verdict (A is-a knave, B is-a knight)
 
 theorem puzzleKnaveOrKnight (A B : Prop) (hA : A ↔ (¬A ∨ B)) :
-    claim% answerKnaveOrKnight [A, B] := sorry
+    claim% answerKnaveOrKnight [A, B] :=
+        match Classical.em A with
+        | .inl a => sorry
+        | .inr na => sorry
 
 /-- You meet A and B.
 A says: "B and I are not the same type."
